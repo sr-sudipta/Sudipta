57a220cc7b0451ffabe0d77d90047a1f5128e18f
sr-sudipta <60208826+sr-sudipta@users.noreply.github.com>
2020-02-06 13:34:27 +0530

    year

---
+++ year.java
@@ -0,0 +1,16 @@
+class sweet {
+int w;
+  sweet( int s)
+  {
+  w=s;
+  }
+  }
+  class year{
+  public static void main(Strings args[]){
+  sweet a =new sweet(5);
+  sweet b =new sweet(10);
+  sweet c =new sweet(15);
+  sweet d =new sweet(1);
+  System.out.println("Weight of sweet a:"+ a.sweet());
+  }
+  }
